522b84c751684210147f7d5cc2b1c243d13189ff
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-11-04 22:35:00 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -3,24 +3,26 @@
 def q1():
   word = input("Input a word: ")
   print(f"Input a word: {word}")
-
+      
 def q2():
   
-  name = input("Input your first name: ")
-  print(f"Input your first name: Hello {name}")
+ first_name = input("Input your first name: ")
+  print(f"Input your first name: Hello {first_name}")
   
 
 def q3():
-
   first_name = input("Input your first name: ")
   last_name = input("Input your last name: ")
   print(f"Input your first name: Input your last name: {last_name} {first_name}")
 
-def q4():
+  
 
+def q4():
   student_1 = input("Input a student: ")
   student_2 = input("Input another student: ")
   print(f"Input a student: Input another student: Your students are {student_1} and {student_2}")
+
+ 
 
 #Do not edit code below this comment
 
